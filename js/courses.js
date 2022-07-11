7c3dcdc0579fffdb7b92f964b6d25738d7825eec
AlanWangsgard <alanwangsgard@byui.edu>
2022-07-11 12:04:27 -0600

    realdata

--- js/courses.js
+++ js/courses.js
@@ -1,9 +1,17 @@
 async function getschools() {
-    const data = await fetch("https://course-tracker-byu.herokuapp.com/schools/").then(res => res.json())
+    const data = await fetch("https://course-tracker-byu.herokuapp.com/courses/valid").then(res => res.json())
     return data
 }
 
+function getParam(param) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get(param);
+}
+
 async function main() {
+    const school = getParam("schoolName")
+    console.log(school)
     const data = await getschools()
     console.log(data)
     const main = document.querySelector("main")
@@ -12,17 +20,17 @@ async function main() {
     data.forEach(element => {
         var p = document.createElement("p")
             // p.textContent = element._id + " " + element.name + " " + element.description
-        var template = `<div class="school-info">
+        if (element.school == school) {
+            var template = `<div class="school-info">
             <section class="school-header">
                 <h3><span class="school-title">"${element.name}"</span></h3>
                 <button>More Info</button>
-                <a href="../courses/index.html?schoolName=${element.name}">Courses</a>
             </section>
             <hr>
             <p><span class="school-description">"${element.description}"</span></p>
         </div>`;
-        main.innerHTML += template
-
+            main.innerHTML += template
+        }
     });
 
 }
