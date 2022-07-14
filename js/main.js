4d51cad5a78ab6c994622c1874f6aa9cd79ad6f9
AlanWangsgard <alanwangsgard@byui.edu>
2022-07-14 10:32:59 -0600

    c

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 async function getData() {
-    info = {
+    var info = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: {
-            "fname": "John",
-            "lname": "Doe",
+            "fname": "Alan",
+            "lname": "W",
             "major": "Computer Science",
             "school": "Brigham Young University",
             "school_id": "1234567890"
         }
     }
-    const data = fetch("https://course-tracker-byu.herokuapp.com/users/")
+    const data = fetch("https://course-tracker-byu.herokuapp.com/users/" + info)
     return data
 }
 
